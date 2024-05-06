4bec8a28ff7f097aacccd8f6b81658d60b8807a3
blehtela <142437787+blehtela@users.noreply.github.com>
2024-05-06 13:01:45 +0300

    newest gamhistos MIX (this is only for MC, so different version currently than used for data)

--- GamHistosMix.C
+++ GamHistosMix.C
@@ -50,8 +50,8 @@ void GamHistosMix() {
   */
 
   //w1
-  GamHistosMixEra("2023","w12"); //make this contain MC without BPix issue
-  GamHistosMixEra("2023-BPix","w12"); //MC accounted for BPix issue
+  GamHistosMixEra("2023","w14"); //make this contain MC without BPix issue
+  GamHistosMixEra("2023-BPix","w14"); //MC accounted for BPix issue
 }
 
 TF1 *_f1p(0);
