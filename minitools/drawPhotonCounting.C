e10c6e40df76479797c899b5c355ba62cf752b3f
blehtela <142437787+blehtela@users.noreply.github.com>
2024-06-10 16:57:20 +0300

    Creating script to plot cross section over integrated luminosity.

--- minitools/drawPhotonCounting.C
+++ minitools/drawPhotonCounting.C
@@ -1,32 +1,43 @@
 // Purpose: Draw jet counting vs photon counting per fill, then vs lumi
 // Based on drawZcountingV2.C by mikko (dot) voutilainen (at) cern (dot) ch
+// Other code to have a look at (was linked here earlier):   From David Walter: https://github.com/CMS-LUMI-POG/ZCounting/blob/Run2/ZHarvester/Plotting/plot_jet_comparison.py
+	
 #include "TFile.h"
 #include "TTree.h"
 #include "TProfile.h"
 #include "TF1.h"
 
 #include "../tdrstyle_mod22.C"
-double minLumi = 500;//1000 pb^{-1}
+//double minLumi = 500;//1000 pb^{-1}
+double minLumi = 1000; //pb^{-1}; one inverse femtobarn per bin
+
+//typedef for lumimap
+typedef std::map<int,double> LumiMap;
+
+LumiMap LoadLumi(string filename);
 
 void drawZcountingV2() {
 
   setTDRStyle();
   TDirectory *curdir = gDirectory;
 
-  TFile *f = new TFile("rootfiles/zcountingV2.root","READ");
+  //TFile *f = new TFile("rootfiles/zcountingV2.root","READ");
+  TFile *f = new TFile("rootfiles/GamHistosFill_data_2024only_w29.root","READ");
   assert(f && !f->IsZombie());
   //TFile *f2 = new TFile("rootfiles/zcountingV2_ecalPrefire_v1.root","READ");
-  TFile *f2 = new TFile("rootfiles/zcountingV2_emuPrefires_v2.root","READ");
-  assert(f2 && !f2->IsZombie());
+  //TFile *f2 = new TFile("rootfiles/zcountingV2_emuPrefires_v2.root","READ");
+  //assert(f2 && !f2->IsZombie());
 
   TTree *t = (TTree*)f->Get("counts");
-  TTree *t2 = (TTree*)f2->Get("counts");
+  //TTree *t2 = (TTree*)f2->Get("counts");
   curdir->cd();
 
   t->Draw("jetcount/reclumj:run>>pj(60000,270000,330000)","(reclumj>0)*reclumj","prof");
+	/*
   t2->Draw("jetcount/reclumj:run>>pjj(60000,270000,330000)","(reclumj>0)*reclumj","prof");
   t2->Draw("zcount/reclumz:run>>pz(60000,270000,330000)","(reclumz>0)*reclumz","prof");
   t2->Draw("run>>hzlum(60000,270000,330000)","reclumz","");
+	
   
   TProfile *pj = (TProfile*)gROOT->FindObject("pj");
   TProfile *pjj = (TProfile*)gROOT->FindObject("pjj");
@@ -36,44 +47,79 @@ void drawZcountingV2() {
   TH1D *hj = pj->ProjectionX("hj");
   TH1D *hjj = pjj->ProjectionX("hjj");
   TH1D *hz = pz->ProjectionX("hz");
+	*/
+
+  TH1D *hgam = (TProfile*)gROOT->FindObject("pr50n"); //testing with this histogram...
+
   
   TF1 *f1 = new TF1("f1","[0]",0,500000);
+
+  hgam->Fit(f1,"QRNW");
+  hgam->Scale(1./f1->GetParameter(0));
+
+ /*
   hj->Fit(f1,"QRNW");
   hj->Scale(1./f1->GetParameter(0));
   hjj->Fit(f1,"QRNW");
   hjj->Scale(1./f1->GetParameter(0));
   hz->Fit(f1,"QRNW");
   hz->Scale(1./f1->GetParameter(0));
+*/
 
   // Create helper histogram for checking IOV boundaries
-  //double viov[] = {270000, 290000, 310000, 330000};
-  //double viov[] = {273158,278777, 278769,284044, 297050,306460, 315257,325172};
-  double viov[] = {273158,278777, 278778,284044, 297050,306460, 315257,325172};
+	double viov[] = {378971, 379412, 380253, 380948, 381384};
+ 
   const int niov = sizeof(viov)/sizeof(viov[0])-1;
   TH1D *hiov = new TH1D("hiov","",niov,viov);
 
-  // From David Walter:
-  // https://github.com/CMS-LUMI-POG/ZCounting/blob/Run2/ZHarvester/Plotting/plot_jet_comparison.py
+ 
+	//start of different runs, see: https://twiki.cern.ch/twiki/bin/viewauth/CMS/PdmVRun3Analysis
   map<int,string> rundict;
-  rundict[273158] = "16BCD";
-  rundict[276831] = "EF";//"16EF";
-  rundict[278820] = "16GH";
-  rundict[297020] = "17B";          
-  rundict[299337] = "C";//17C";          
-  rundict[302030] = "D";//"17D";          
-  rundict[303435] = "E";//"17E";          
-  rundict[304911] = "F";//"17F";          
-  rundict[315252] = "18A";  
-  rundict[316998] = "B";//"18B";  
-  rundict[319313] = "C";//"18C";  
-  rundict[320394] = "D";//"18D";
-  rundict[325172] = "end"; // the end
+  rundict[378971] = "24B";
+  rundict[379412] = "24C";
+  rundict[380253] = "24D";
+  rundict[380948] = "24Ev1";          
+  rundict[381384] = "24Ev2";          
+
+
+	//the lumifiles (from brilcalc, used hybrid json for w29)
+  //Get recorded luminosity for different triggers, pb=in picobarn:
+	// run --> lumi in /pb
+  LumiMap lumi30, lumi50, lumi110, lumi200;
+  lumi30 = LoadLumi("files/lumi2024_hybridjson-w29_photon30eb_pb.csv");
+  lumi50 = LoadLumi("files/lumi2024_hybridjson-w29_photon50eb_pb.csv");
+  lumi110 = LoadLumi("files/lumi2024_hybridjson-w29_photon110eb_pb.csv");
+  lumi200 = LoadLumi("files/lumi2024_hybridjson-w29_photon200_pb.csv");
+
+ 
+	//testing this only for 50 GeV trigger --> TO DO: loop through all of them.
   
-  // Determine new binning to have >1/fb per bin
+  // Determine new binning to have >1/fb per bin 
+	//--> need to sum runs until 1 /fb is filled, then start next bin (note, my brilcalc output is in pb, so need to cross 1000 /pb)
+	//go through entries in the photon count histogram (i.e. through all runs)
+  vector<int> ix;
+  vector<double> x;
+	double currlum = 0; //start with 0 integrated luminosity, always set to zero after 1/fb is full (i.e. when going to next new bin)
+	int runnum; 
+
+	//shouldn't this be better a while loop?
+  for (int i = 1; i != hgam->GetNbinsX()+1; ++i) { //go through all runs
+		runnum = hgam->GetBinCenter(i);
+		currlum += lumi30[runnum]; //add integrated luminosity for current run
+
+	 	if(currlum>1000.0){ //hit the 1 /fb, finish this bin by setting its edge (runnum) to x vector; what if currlum%1000.0 = 0
+			x.push_back(runnum); //this run will be a bin edge
+			currlum == 0; //set lumi in new bin again to zero
+		}
+
+	}
+
+
+
   // TBD: check if IOV boundary to not go across those
   vector<int> ix;
   vector<double> x;
-  for (int i = 1; i != hzlum->GetNbinsX()+1; ++i) {
+  for (int i = 1; i != hzlum->GetNbinsX()+1; ++i) { //could i use just ANY histogram that has run# as x-axis here?
 
     // Sum up active runs
     if (hzlum->GetBinContent(i)>0) {
@@ -83,26 +129,26 @@ void drawZcountingV2() {
       
       // Set starting point to first active run
       if (x.size()==0) {
-	x.push_back(thisRun);
-	ix.push_back(i);
+					x.push_back(thisRun);
+					ix.push_back(i);
       }
 
       // Check if latest bins add up to >1/fb, or hitting IOV boundary
       bool isBoundary = (hiov->FindBin(thisRun) != hiov->FindBin(nextRun));
       int ix1 = ix[ix.size()-1];
-      double sumLumi = hzlum->Integral(ix1,i); // in /pb
+      double sumLumi = hzlum->Integral(ix1,i); // in /pb --> instead of this, should sum lumi values from my .csv?
       if (sumLumi>minLumi || isBoundary) {
 
-	// If boundary and not full 1/fb, replace previous upper edge
-	if (isBoundary && sumLumi<minLumi) {
-	  x[x.size()-1] = nextRun;
-	  ix[ix.size()-1] = i+1;
-	}
-	// Otherwise just start next bin
-	else {
-	  x.push_back(nextRun);
-	  ix.push_back(i+1);
-	}
+					// If boundary and not full 1/fb, replace previous upper edge
+					if (isBoundary && sumLumi<minLumi) {
+						x[x.size()-1] = nextRun;
+						ix[ix.size()-1] = i+1;
+					}
+					// Otherwise just start next bin
+					else {
+						x.push_back(nextRun);
+						ix.push_back(i+1);
+					}
       }
     }
   } // hzlum
@@ -238,8 +284,11 @@ void drawZcountingV2() {
   tex->DrawLatex(0.60,0.26,"Jet veto map (per year)");
 
   gPad->RedrawAxis();
-  c1->SaveAs("pdf/drawZcountingV2.pdf");
+	c1->SaveAs("pdf/drawPhotonCounting.pdf");
 
+
+// ---------------- TO DO: Adapt this also to photon+jet  --------------------------------------//
+/* 
 
   // Ratio of jet and Z counting
   TH1D *h2 = tdrHist("h2","#sigma_{jet} / #LT#sigma_{jet}#GT)"
@@ -326,7 +375,69 @@ void drawZcountingV2() {
   }
 
   gPad->RedrawAxis();
-  c2->SaveAs("pdf/drawZcountingV2_ratio.pdf");
-
+  //c2->SaveAs("pdf/drawZcountingV2_ratio.pdf");
+c2->SaveAs("pdf/drawPhotonCounting_ratio.pdf");
   
 } // drawZcountingV2
+*/
+
+
+LumiMap LoadLumi(string filename){
+    //the file(s) where lumi is stored for trigger
+    fstream lumifile; //pointer to file
+
+    //read in the .csv file for, e.g. 50EB, photon trigger
+    //TFile* lumifile =;
+    //lumifile.open("files/lumi2024_378981_380115_Golden_photon50eb.csv", ios::in);
+    lumifile.open(filename, ios::in);
+    LumiMap lumi;
+
+    //reading (use vector of strings)
+    vector<string> row;
+    string currline, colentry, temp;
+
+    //read the file
+    int nrows=0;
+    while(getline(lumifile, currline)){
+        row.clear();
+
+        //break words
+        stringstream str(currline);
+
+        //delimiter
+        char delim = ',';
+        //go through columns of this row
+        while(getline(str, colentry, delim)){
+            //add current column's entry to row vector
+            row.push_back(colentry);
+        }
+
+        //show FIRST and LAST entry of row
+        if(row.at(0)[0]!='#'){ //check if first entry isn't starting with # (as these would be comments)
+            nrows+=1; //increase valid row counter by one
+            //cout << "row (first element = run number): " << row.at(0) << endl; // first element
+            //cout << "row (last element = recorded lumi): " << row.back() << endl;
+            
+            stringstream runstring;
+            int runnum;
+            runstring << row.at(0);
+            runstring >> runnum;
+
+            stringstream lumistring;
+            double currlum; 
+            lumistring << row.back();
+            lumistring >> currlum;
+
+            lumi.emplace(runnum, currlum); //fill value to lumimap
+        }
+    }//stop reading file
+
+    //print the lumi map
+    /*
+    for(const auto& elem : lumi50){ //iterator through map's elements
+        cout << "run#: " << elem.first << " lumi: " << elem.second << " " << endl;
+    }
+    */
+
+    return lumi; //return the lumimap
+}//end of lumimap
