eac8fb4bf149f5cf570365e458d9334dd612918e
blehtela <142437787+blehtela@users.noreply.github.com>
2025-01-15 11:50:50 +0100

    w44: updated pu summary creation code to include 2022, 2023, and 2024 for a given version (now: w44). Still needs to be tested.

--- SummaryPileupHistos.C
+++ SummaryPileupHistos.C
@@ -7,12 +7,13 @@
 
 
 
-void SummaryPileupHistos(string ver = "w43") {
+void SummaryPileupHistos(string ver = "w44") {
     TChain chain("Events");
 
 		//list of data eras to be saved to summary file:
 		//string data_eras[] = {"2024B", "2024C", "2024D", "2024Ev1", "2024Ev2", "2024F", "2024G", "2024H", "2024I", "2024GH"}; //for eras in 2024
-		string data_eras[] = {"2024Bnib1", "2024Cnib1", "2024Dnib1", "2024Ev1nib1", "2024Ev2nib1", "2024Fnib1", "2024Fnib2", "2024Fnib3", "2024Gnib1", "2024Gnib2", "2024Hnib1", "2024Inib1"}; //for nibs in 2024
+		//string data_eras[] = {"2024Bnib1", "2024Cnib1", "2024Dnib1", "2024Ev1nib1", "2024Ev2nib1", "2024Fnib1", "2024Fnib2", "2024Fnib3", "2024Gnib1", "2024Gnib2", "2024Hnib1", "2024Inib1"}; //for nibs in 2024
+		string data_eras[] = {"2022C", "2022D", "2022E", "2022F", "2022G", "2023B", "2023Cv123", "2023Cv4", "2023D", "2024B", "2024C", "2024D", "2024Ev1", "2024Ev2", "2024F", "2024G", "2024H", "2024I", "2024GH"}; //for eras in 2024, 2023, 2022
 		const int neras = sizeof(data_eras)/sizeof(data_eras[0]); //.size();
 
 
@@ -30,12 +31,39 @@ void SummaryPileupHistos(string ver = "w43") {
 		// MONTE CARLO PU DISTRIBUTIONS  --> read from files and save to summary file.
 		//TFile *mfp8 = new TFile(Form("pileup/2024/pileup_winter2024P8.root")); //winter2024P8
 		//TFile *mfqcd = new TFile(Form("pileup/2024/pileup_2024QCD.root")); //2024QCD
+		string mc2022[] = {"2022P8", "2022EEP8", "2022QCD", "2022EEQCD"}; 		// mc for 2022 (both gamjet and qcd)
+		string mc2023[] = {"2023P8", "2023P8-BPix", "2023QCD", "2023QCD-BPix"};		// mc for 2023 (both gamjet and qcd)
+		string mc2024[] = {"winter2024P8", "2024QCD"};					// mc for 2024 (both gamjet and qcd)
 
+		for(int i=0; i<neras; ++i){
+			string mclist[](0);
+			if(TString(data_eras[i].c_str()).Contains("2022")){ mclist = mc2022; } 
+			else if(TString(data_eras[i].c_str()).Contains("2023")){ mclist = mc2023; }
+			else if(TString(data_eras[i].c_str()).Contains("2024")){ mclist = mc2024; }
+			const int nmcsets = sizeof(mclist)/sizeof(mclist[0]);
+
+			for(int j=0; j<nmcsets; ++j){
+				TFile *mcfile = new TFile(Form("pileup/pileup_%s_%s.root", mclist[j].c_str(), ver.c_str())); //open file for this mc set (contains pu distribution)
+				//MC PU pre-reweighting
+				TH1D *mc_pu = (TH1D*)mcfile->Get(Form("pileup_%s", mclist[j].c_str()));
+
+				//SAVING original distribution, and scaled distribution
+				//get number of MC events in pu profile
+				Float_t mc_nevt = mc_pu->Integral();
+
+				summaryFile->cd();
+				mc_pu->Write(); 	//write pu distribution for this MC version (not normalised)
+
+				mc_pu->Scale(1./mc_nevt); //normalise to 1
+				mc_pu->Write(Form("pileup_%s_scaled", mclist[j].c_str())); //write normalised pu dist for MC (BEFORE reweighting)
+			}
+		}
+
+
+		/*
 		//needed a workaround on lxplus...have those on vulcan...so here read from previous summary (w41, no changes to mc): TODO: FIX THIS
-		TFile *mfp8 = new TFile(Form("pileup/2024/pu_summary_w41.root")); //winter2024P8
-		TFile *mfqcd = new TFile(Form("pileup/2024/pu_summary_w41.root")); //2024QCD
-
-
+		//TFile *mfp8 = new TFile(Form("pileup/2024/pu_summary_w41.root")); //winter2024P8
+		//TFile *mfqcd = new TFile(Form("pileup/2024/pu_summary_w41.root")); //2024QCD
 
 		//MC PU pre-reweighting
 		TH1D *mcp8_pu = (TH1D*)mfp8->Get(Form("pileup_winter2024P8"));
@@ -56,6 +84,7 @@ void SummaryPileupHistos(string ver = "w43") {
 
 		mcqcd_pu->Scale(1./mcqcd_nevt); //normalise to 1
 		mcqcd_pu->Write(Form("pileup_2024QCD_scaled")); //write normalised pu dist for MC (BEFORE reweighting)
+		*/
 
 
 
@@ -69,8 +98,13 @@ void SummaryPileupHistos(string ver = "w43") {
 				for(int j=0; j<ntrigs; ++j){
 						//TFile *df = new TFile(Form("/media/Duo2/bschilli/pileup2024_%s/MyDataPileupHistogram_%s_%s_%s.root", 
 						//				ver.c_str(), data_eras[i].c_str(), trg[j].c_str(), ver.c_str()),"READ"); //test it with w41
-						TFile *df = new TFile(Form("/eos/user/b/blehtela/pileup_%s/MyDataPileupHistogram_%s_%s_%s.root", 
-										ver.c_str(), data_eras[i].c_str(), "photon50eb", ver.c_str()),"READ"); //only 50gev trig, and named wrongly...
+						//TFile *df = new TFile(Form("/eos/user/b/blehtela/pileup_%s/MyDataPileupHistogram_%s_%s_%s.root", 
+						//				ver.c_str(), data_eras[i].c_str(), "photon50eb", ver.c_str()),"READ"); //only 50gev trig, and named wrongly...
+				
+						//with year folder now
+						string year = data_eras[i].c_str().substr(0,4);
+						TFile *df = new TFile(Form("/eos/user/b/blehtela/pileup_%s/%s/MyDataPileupHistogram_%s_%s_%s.root", 
+										ver.c_str(), data_eras[i].c_str(), year.c_str(), "photon50eb", ver.c_str()),"READ"); //only 50gev trig, and named wrongly...
 
 						assert(df);
 
