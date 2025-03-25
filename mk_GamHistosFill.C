107b3eb1769d8490f93b5f2765905065098c6deb
blehtela <142437787+blehtela@users.noreply.github.com>
2025-03-25 22:11:14 +0100

    Changed to version w48 and added summer2024QCD to the addQCD boolian.

--- mk_GamHistosFill.C
+++ mk_GamHistosFill.C
@@ -46,7 +46,7 @@ R__LOAD_LIBRARY(GamHistosFill_C.so)
 #endif
 
 
-void mk_GamHistosFill(string dataset = "X", string puera = "", string version = "w47") { //using w-version names for my code (Bettina).
+void mk_GamHistosFill(string dataset = "X", string puera = "", string version = "w48") { //using w-version names for my code (Bettina).
 //void mk_GamHistosFill(string inputlist = "X", string version = "w36") { //using w-version names for my code (Bettina). TO DO
 
 
@@ -103,7 +103,8 @@ void mk_GamHistosFill(string dataset = "X", string puera = "", string version = 
                 dataset=="2023P8X" || dataset=="2023P8-BPixX"|| //for my test wX23
 		dataset=="2023P8" || //);// || dataset=="2023QCD");
   		dataset=="2023P8-BPix" || //added the BPix MC files
-  		dataset=="winter2024P8" || dataset=="summer2024P8" || dataset=="winter2024P8-test" || dataset=="summer2024P8-test" || 
+  		dataset=="winter2024P8" || dataset=="summer2024P8" ||  dataset=="summer2024P8" ||
+			dataset=="winter2024P8-test" || dataset=="summer2024P8-test" || 
 			dataset=="winter2024P8-v14" || //winter 2024 madgraph p8 (added on 07.08.2024)
 			dataset=="winter2024P8a" || dataset=="winter2024P8b" || dataset=="winter2024P8c"); //winter 2024 madgraph p8 (added on 07.08.2024)
 
@@ -115,7 +116,8 @@ void mk_GamHistosFill(string dataset = "X", string puera = "", string version = 
 		 dataset=="2023QCD" || dataset=="2023QCD-BPix" ||  //added BPix QCD MC
 		 dataset=="2024QCD" || dataset=="2024QCD-v14" || //); //added BPix QCD MC
 		dataset=="2024QCDa" || dataset=="2024QCDb" || dataset=="2024QCDc" || dataset=="2024QCDd" || 
-		dataset=="2024QCDe" || dataset=="2024QCDf"); //these do use v14 puppi despite not in name here
+		dataset=="2024QCDe" || dataset=="2024QCDf" || //these do use v14 puppi despite not in name here
+		dataset=="summer2024QCD"); //added 25.03.2025 (w48)
 
 
   //cout << "Clean old shared objects and link files" << endl << flush;
