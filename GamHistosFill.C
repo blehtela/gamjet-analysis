3b565d851df14644c6803b3f5980941a26b79cea
blehtela <142437787+blehtela@users.noreply.github.com>
2025-04-14 10:59:41 +0200

    w49: Adding histograms for the energy fraction balance EFB to update the PF composition plot over run number (photon50EB trigger only so far).

--- GamHistosFill.C
+++ GamHistosFill.C
@@ -1452,18 +1452,22 @@ void GamHistosFill::Loop()
   TProfile *pr110chf = new TProfile("pr110chf",";Run;CHF;",histnx,xmin,xmax);
   TProfile *pr230chf = new TProfile("pr230chf",";Run;CHF;",histnx,xmin,xmax);
   TProfile *prg1chf = new TProfile("prg1chf",";Run;CHF;",histnx,xmin,xmax);
+  TProfile *pr50efb_chf = new TProfile("pr50efb_chf","p_{T,rawj} #cdot CHF / p_{T,#gamma};Run;EFB CHF;",histnx,xmin,xmax);
   //
   TProfile *pr30nhf = new TProfile("pr30nhf",";Run;NHF;",histnx,xmin,xmax);
   TProfile *pr50nhf = new TProfile("pr50nhf",";Run;NHF;",histnx,xmin,xmax);
   TProfile *pr110nhf = new TProfile("pr110nhf",";Run;NHF;",histnx,xmin,xmax);
   TProfile *pr230nhf = new TProfile("pr230nhf",";Run;NHF;",histnx,xmin,xmax);
   TProfile *prg1nhf = new TProfile("prg1nhf",";Run;NHF;",histnx,xmin,xmax);
+  TProfile *pr50efb_nhf = new TProfile("pr50efb_nhf","p_{T,rawj} #cdot NHF / p_{T,#gamma};Run;EFB NHF;",histnx,xmin,xmax);
   //
   TProfile *pr30nef = new TProfile("pr30nef",";Run;NHF;",histnx,xmin,xmax);
   TProfile *pr50nef = new TProfile("pr50nef",";Run;NHF;",histnx,xmin,xmax);
   TProfile *pr110nef = new TProfile("pr110nef",";Run;NHF;",histnx,xmin,xmax);
   TProfile *pr230nef = new TProfile("pr230nef",";Run;NHF;",histnx,xmin,xmax);
   TProfile *prg1nef = new TProfile("prg1nef",";Run;NHF;",histnx,xmin,xmax);
+  TProfile *pr50efb_nef = new TProfile("pr50efb_nef","p_{T,rawj} #cdot NEF / p_{T,#gamma};Run;EFB NEF;",histnx,xmin,xmax);
+
 
   // - - - - - - - high jet eta investigations - - - - - - - - //
   //histograms for investigation of high jet eta (couldn't these be added as a loop???)
@@ -3571,6 +3575,12 @@ void GamHistosFill::Loop()
       pr50chf->Fill(run, Jet_chHEF[iJet], w);
       pr50nhf->Fill(run, Jet_neHEF[iJet], w);
       pr50nef->Fill(run, Jet_neEmEF[iJet], w);
+
+      //energy fraction balance
+      pr50efb_chf->Fill(run, rawjet.Pt()*Jet_chHEF[iJet]*(1./gam.Pt()), w); //charged hadron energy fraction
+      pr50efb_nhf->Fill(run, rawjet.Pt()*Jet_neHEF[iJet]*(1./gam.Pt()), w); //neutral hadron energy fraction
+      pr50efb_nef->Fill(run, rawjet.Pt()*Jet_neEmEF[iJet]*(1./gam.Pt()), w); //neutral electromagnetic energy fraction
+
 
     if(abs(gam.Eta())>0.8){
       pr50m_eta08hi->Fill(run, mpf, w); //should this have the eta cut at 1.3 or not? YES, with cut
