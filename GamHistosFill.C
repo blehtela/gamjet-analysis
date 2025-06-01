d9e00e9e52fb162ef91c816360b0c0714c25fba2
blehtela <142437787+blehtela@users.noreply.github.com>
2025-06-01 23:43:12 +0200

    Add date for tests on 02.06.2025.

--- GamHistosFill.C
+++ GamHistosFill.C
@@ -1061,7 +1061,7 @@ void GamHistosFill::Loop()
   // Create histograms. Copy format from existing files from Lyon
   // Keep only histograms actually used by global fit (reprocess.C)
   TDirectory *curdir = gDirectory;
-  TFile *fout = new TFile(Form("rootfiles/GamHistosFill_%s_%s_pu-%s_noL2L3Res_%s_28May2025.root", //added date just for tests today
+  TFile *fout = new TFile(Form("rootfiles/GamHistosFill_%s_%s_pu-%s_noL2L3Res_%s_02Jun2025.root", //added date just for tests today
 			       isMC ? "mc" : "data",
 			       dataset.c_str(), puera.c_str(), version.c_str()), //UPDATED
 			  "RECREATE");
