233ed5adfb79f0647df94fc849a52a3f3a26c695
blehtela <142437787+blehtela@users.noreply.github.com>
2025-08-06 10:12:56 +0200

    Just a comment on versioning.

--- mk_GamHistosFill.C
+++ mk_GamHistosFill.C
@@ -46,7 +46,7 @@ R__LOAD_LIBRARY(GamHistosFill_C.so)
 #endif
 
 
-void mk_GamHistosFill(string dataset = "X", string puera = "", string version = "w56") { //using w-version names for my code (Bettina).
+void mk_GamHistosFill(string dataset = "X", string puera = "", string version = "w56") { //using w-version names for my code (Bettina)., we are at w56, w57 is for WITHOUT L2L3Res 2025 for dpnote
 //void mk_GamHistosFill(string inputlist = "X", string version = "w36") { //using w-version names for my code (Bettina). TO DO
 
 
